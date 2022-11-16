ee798effda1b2b58e5cea46e47af9516e33a2efc
OmgFaaruq <faaruq.mansray@hotmail.co.uk>
2022-11-16 12:35:44 +0000

    Look at me go

--- Mini_Project.py
+++ Mini_Project.py
@@ -112,7 +112,7 @@ while True:
                         print('Product index is invalid. Please try again')
                         print()
                     else:
-                        product_name = input("Product Name: ")
+                        product_name = input("Updated Product Name: ")
                         product_list[product_index] = product_name
                         print("\nProduct list: ", product_list)
                         print()
@@ -123,6 +123,7 @@ while True:
         3. Update an exisiting product \n\
         4. Delete a product \n\
         >> "))
+                        break
 
             # delete product
             elif product_menu_option == 4:
@@ -132,10 +133,8 @@ while True:
                 while True:
                     try:
                         print()
-                        product_index = int(input("Product Index: "))
+                        product_index = int(input("Please input the index of the product you would like to delete: "))
                         del product_list[product_index]
-                        if product_index > len(product_list)-1:
-                            raise Exception
                     except Exception:
                         print()
                         print('Product index is invalid. Please try again')
@@ -150,6 +149,7 @@ while True:
         3. Update an exisiting product \n\
         4. Delete a product \n\
         >> "))
+                        break
 
         # END of Prodcut Menu Option INNER LOOP <---------
 
@@ -208,24 +208,33 @@ while True:
                 for (count, item) in enumerate(couriers_list):
                     print(count, item, sep = " ")
                 print()
-                courier_index_value = int(input('Please input the index of the exisiting courier you would like to UPDATE: '))
-                courier_to_update = couriers_list[courier_index_value]
-                print()
-                print(f"You have selected '{courier_to_update}' to update")
-                print()
-                new_courier_name = input('Please type out the new courier\'s name: ')
-                couriers_list[courier_index_value] = new_courier_name
-                print()
-                for (count, item) in enumerate(couriers_list):
-                    print(count, item, sep = " ")
-                print()
-                courier_menu_option = int(input("Please select one of the following options: \n\
+
+                while True:
+                    try:
+                        courier_index_value = int(input('Please input the index of the exisiting courier you would like to UPDATE: '))
+                        courier_to_update = couriers_list[courier_index_value]
+                        print()
+                    except Exception:
+                        print()
+                        print('Courier Index is invalid. Please try again.')
+                        print()
+                    else:
+                        print(f"You have selected '{courier_to_update}' to update")
+                        print()
+                        new_courier_name = input('Please type out the new courier\'s name: ')
+                        couriers_list[courier_index_value] = new_courier_name
+                        print()
+                        for (count, item) in enumerate(couriers_list):
+                            print(count, item, sep = " ")
+                        print()
+                        courier_menu_option = int(input("Please select one of the following options: \n\
         0. Return to main menu \n\
         1. View couriers list \n\
         2. Create new courier \n\
         3. Update existing courier \n\
         4. Delete courier \n\
         >> "))
+                    break
 
             # delete courier
             elif courier_menu_option == 4:
@@ -233,17 +242,26 @@ while True:
                 for (count, item) in enumerate(couriers_list):
                     print(count, item, sep = " ")
                 print()
-                courier_index_value = int(input('Please input the index of the exisiting courier you would like to DELETE: '))
-                del couriers_list[courier_index_value]
-                print('\nCouriers List: ', couriers_list)
-                print()
-                courier_menu_option = int(input("Please select one of the following options: \n\
+
+                while True:
+                    try:
+                        courier_index_value = int(input('Please input the index of the exisiting courier you would like to DELETE: '))
+                        del couriers_list[courier_index_value]
+                    except Exception:
+                        print()
+                        print('Courier Index is invalid. Please try again.')
+                        print()
+                    else:
+                        print('\nCouriers List: ', couriers_list)
+                        print()
+                        courier_menu_option = int(input("Please select one of the following options: \n\
         0. Return to main menu \n\
         1. View couriers list \n\
         2. Create new courier \n\
         3. Update existing courier \n\
         4. Delete courier \n\
         >> "))
+                break
         
         # END of INNER COURIER LOOP
 
@@ -320,20 +338,28 @@ while True:
                 for (count, item) in enumerate(orders_list):
                     print(count, item, sep = ' ')
                 print()
-                orders_list_index_input = int((input("Please enter the order index value: ")))
-                order_to_update = orders_list[orders_list_index_input]
-                print()
-                print(f"You have selected {order_to_update} to update")
-                print()
-                for (count, item) in enumerate(order_status_list):
-                    print(count, item, sep = ' ')
-                print()
-                new_order_status_index = int(input('Please select an index from the Order Status List: '))
-                new_order_status = order_status_list[new_order_status_index]
-                order_to_update['Order Status'] = new_order_status
-                print('\nUpdated Order: ', order_to_update)
-                print()
-                orders_menu_section = int(input("Please select one of the following options: \n\
+
+                while True:
+                    try:
+                        orders_list_index_input = int((input("Please enter the order index value: ")))
+                        order_to_update = orders_list[orders_list_index_input]
+                    except Exception:
+                        print()
+                        print('Order Index is invalid. Please try again.')
+                        print()
+                    else:
+                        print()    
+                        print(f"You have selected {order_to_update} to update")
+                        print()
+                        for (count, item) in enumerate(order_status_list):
+                            print(count, item, sep = ' ')
+                        print()
+                        new_order_status_index = int(input('Please select an index from the Order Status List: '))
+                        new_order_status = order_status_list[new_order_status_index]
+                        order_to_update['Order Status'] = new_order_status
+                        print('\nUpdated Order: ', order_to_update)
+                        print()
+                        orders_menu_section = int(input("Please select one of the following options: \n\
         0. Return to main menu \n\
         1. View the orders dictionary \n\
         2. Create a new order \n\
@@ -341,60 +367,79 @@ while True:
         4. Update exisiting order \n\
         5. Delete order \n\
         >> "))
+                        break
                 
             # update existiting order:
             elif orders_menu_section == 4:
+                print()
                 for (count, item) in enumerate(orders_list):
                     print(count, item, sep = ' ')
                 print()
-                orders_list_index_input = int((input("Please enter the order index value: ")))
-                order_to_update = orders_list[orders_list_index_input]
-                print()
-                print(f'You have selected {order_to_update} to update')
-                print()
-                updated_customer_name = input('Please input updated customer name, otherwise click Enter: ')
-                print()
-                updated_customer_address = input('Please input updated customer address, otherwise click Enter: ')
-                print()
-                updated_customer_phone = input('Please input updated customer phone, otherwise click Enter: ')
-                for key, value in order_to_update.items():
-                    if key == 'Customer Name':
-                        if not updated_customer_name:
-                            continue
-                        else:
-                            order_to_update[key] = updated_customer_name
-                    if key == 'Customer Address':
-                        if not updated_customer_address:
-                            continue
-                        else:
-                            order_to_update[key] = updated_customer_address
-                    if key == 'Customer Phone':
-                        if not updated_customer_phone:
-                            continue
-                        else:
-                            order_to_update[key] = updated_customer_phone
-                print(order_to_update)
-                print()
-                orders_menu_section = int(input("Please select one of the following options: \n\
+                
+                while True:
+                    try:
+                        orders_list_index_input = int((input("Please enter the order index value: ")))
+                        order_to_update = orders_list[orders_list_index_input]
+                        print()
+                    except Exception:
+                        print()
+                        print('Order Index is invalid. Please try again.')
+                        print()
+                    else:
+                        print(f'You have selected {order_to_update} to update')
+                        print()
+                        updated_customer_name = input('Please input updated customer name, otherwise click Enter: ')
+                        print()
+                        updated_customer_address = input('Please input updated customer address, otherwise click Enter: ')
+                        print()
+                        updated_customer_phone = input('Please input updated customer phone, otherwise click Enter: ')
+                        for key, value in order_to_update.items():
+                            if key == 'Customer Name':
+                                if not updated_customer_name:
+                                    continue
+                                else:
+                                    order_to_update[key] = updated_customer_name
+                            if key == 'Customer Address':
+                                if not updated_customer_address:
+                                    continue
+                                else:
+                                    order_to_update[key] = updated_customer_address
+                            if key == 'Customer Phone':
+                                if not updated_customer_phone:
+                                    continue
+                                else:
+                                    order_to_update[key] = updated_customer_phone
+                        print(order_to_update)
+                        print()
+                        orders_menu_section = int(input("Please select one of the following options: \n\
         0. Return to main menu \n\
         1. View the orders dictionary \n\
         2. Create a new order \n\
         3. Update existing order status \n\
         4. Update exisiting order \n\
         5. Delete order \n\
-        >> ")) 
+        >> "))
+                    break
 
             # delete order 
             elif orders_menu_section == 5:
                 for (count, item) in enumerate(orders_list):
                     print(count, item, sep = " ")
                 print()
-                orders_list_index_input = int((input("Please enter the order index value: ")))
-                del orders_list[orders_list_index_input]
-                print()
-                print('Orders: ', orders_list)
-                print()
-                orders_menu_section = int(input("Please select one of the following options: \n\
+
+                while True:
+                    try:
+                        orders_list_index_input = int((input("Please enter the order index value: ")))
+                        del orders_list[orders_list_index_input]
+                    except Exception:
+                        print()
+                        print('Order Index is invalid. Please try again.')
+                        print()
+                    else:
+                        print()
+                        print('Orders: ', orders_list)
+                        print()
+                        orders_menu_section = int(input("Please select one of the following options: \n\
         0. Return to main menu \n\
         1. View the orders dictionary \n\
         2. Create a new order \n\
@@ -402,7 +447,7 @@ while True:
         4. Update exisiting order \n\
         5. Delete order \n\
         >> "))
-
+                    break
         #END of ORDER MENU INNER loop <---------
 
 
